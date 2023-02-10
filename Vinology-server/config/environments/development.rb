afae383db0e3d67f5daa867d9b600a96992bcd59
ccdaniele <daniel.calderon@datadoghq.com>
2023-02-10 13:55:26 -0500

    v.1.1.3 - Allowing dynamyc host connection

--- Vinology-server/config/environments/development.rb
+++ Vinology-server/config/environments/development.rb
@@ -51,7 +51,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  # Allows dinamy host connection
+  config.hosts << /[a-z0-9]+\.c9users\.io/
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
